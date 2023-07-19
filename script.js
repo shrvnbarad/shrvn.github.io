8c70e0ea6e8859bad5dac10ebd5a5c1961df4fd9
shrvnbarad <shravan21barad@gmail.com>
2023-07-19 12:12:15 +0530

    changes

---
+++ script.js
@@ -0,0 +1,26 @@
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("Slides");
+  if (n > slides.length) {
+    slideIndex = 1
+}
+  if (n < 1) {
+    slideIndex = slides.length
+}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+}
+  slides[slideIndex-1].style.display = "block";  
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+  
+  function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+  
